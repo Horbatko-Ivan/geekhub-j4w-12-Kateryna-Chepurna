df93b1117d9615ecf0a1661317a2b2e32c8f0e58
Kateryna-Chepurna <chep.katerina@gmail.com>
2022-10-22 14:27:55 +0300

    L01 Display "Hello World!" message in main class

--- Homework/java-core/01-Introduction/src/main/java/edu/geekhub/homework/ApplicationStarter.java
+++ Homework/java-core/01-Introduction/src/main/java/edu/geekhub/homework/ApplicationStarter.java
@@ -1,6 +1,12 @@
 package edu.geekhub.homework;
 
+/**
+ * ApplicationStarter app.
+ * Print "Hello world!" in console.
+ */
 public class ApplicationStarter {
+    public static void main(String[] args) {
 
-    // Write code here :)
+        System.out.println("Hello World!");
+    }
 }
